d53cf8c62f6bc485e4e1d4b9587fe9fd4dfb3c12
algorythmist <dimitri@tecacet.com>
2019-09-03 13:12:25 -0700

    more tests and coverage

--- src/test/java/com/tecacet/jflat/impl/FixedWidthLineMergerTest.java
+++ src/test/java/com/tecacet/jflat/impl/FixedWidthLineMergerTest.java
@@ -2,7 +2,6 @@ package com.tecacet.jflat.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.tecacet.jflat.exception.FieldTooWideException;
 import com.tecacet.jflat.exception.LineMergerException;
@@ -20,17 +19,25 @@ class FixedWidthLineMergerTest {
     @Test
     public void testTooManyFields()  {
         FixedWidthLineMerger merger = new FixedWidthLineMerger(new int[] { 2, 5 });
-        Assertions.assertThrows(TooManyFieldsException.class, () -> {
+        try {
             merger.makeLine(new String[]{"12", "12345", "123456"});
-        });
+        } catch (TooManyFieldsException e) {
+            assertEquals(3, e.getFieldCount());
+            assertEquals(2, e.getMaxFieldCount());
+            assertEquals("Too many fields.  Got: 3, Maximum: 2.", e.getMessage());
+        }
     }
 
     @Test
     public void testFieldTooWide() throws LineMergerException {
         FixedWidthLineMerger merger = new FixedWidthLineMerger(new int[] { 2, 5 });
-        Assertions.assertThrows(FieldTooWideException.class, () -> {
+       try {
             merger.makeLine(new String[]{"12", "123456"});
-        });
+        } catch (FieldTooWideException e) {
+           assertEquals("123456", e.getFieldValue());
+           assertEquals(5, e.getMaxFieldWidth());
+           assertEquals("Value '123456' is too wide.  Actual width: 6, Maximum width: 5.", e.getMessage());
+       }
     }
 
     @Test
